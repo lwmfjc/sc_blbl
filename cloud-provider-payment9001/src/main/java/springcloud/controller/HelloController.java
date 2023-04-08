2e1357ba77d762767a9697d14ec61390bc7c80a1
lwmfjc <lwmfjc@gmail.com>
2023-04-08 10:48:13 +0800

    springboot-rocketmq 集成

--- cloud-provider-payment9001/src/main/java/springcloud/controller/HelloController.java
+++ cloud-provider-payment9001/src/main/java/springcloud/controller/HelloController.java
@@ -1,6 +1,8 @@
 package springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,15 @@ public class HelloController {
     @RequestMapping("hello")
     public String hello(){
         log.info("我被调用了");
+        return "hello world"+serverPort;
+    }
+    @Autowired
+    private RocketMQTemplate template;
+
+    @RequestMapping("rocketmq")
+    public String rocketmq(){
+        log.info("我被调用了-rocketmq");
+        template.convertAndSend("mytopic-ly","hello1231");
         return "hello world"+serverPort;
     }
 }
